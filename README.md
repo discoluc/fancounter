708e3bf9ca802a569a6ef90aef7f012a8b60e492
snikcers <snikcers@gmail.com>
2022-09-16 20:45:03 +0200

    ESPAsyncWebserver and HTML to PROGMEM

--- README.md
+++ README.md
@@ -14,11 +14,12 @@ The goal is to build a social media fan counter using an ESP8266 and a 32 x 8 Fu
 - [x] Using WifiManager to set up Wifi
 - [x] Opening webserver for basic setting display
 - [ ] Make Webpage for entering the api key and resetting the wifi creds
+    - using ESPAsynWebserver
 - [ ] Make save api key in SPIFFS
 - [ ] Integrate Facebook API Request
 
     <sub><sup>https://graph.facebook.com/v14.0/{ownuserprofileid}?fields=followers_count&access_token={apikey}</sub></sup>
-    
+
     <sub><sup>https://graph.facebook.com/v14.0/{ownuserprofileid}?fields=business_discovery.username({seconduser})%7Bfollowers_count%7D&access_token={apikey}</sub></sup>
 
 
--- fancounter.ino
+++ fancounter.ino
@@ -7,12 +7,42 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <ESPAsyncTCP.h> // https://github.com/me-no-dev/ESPAsyncTCP
 
-// Set web server port number to 80
-WiFiServer server(80);
+// BugFix if using ESPAsyncWebserver and WiFiManager --> https://github.com/me-no-dev/ESPAsyncWebServer/issues/418#issuecomment-667976368
+#define WEBSERVER_H
+#include <ESPAsyncWebServer.h> // https://github.com/me-no-dev/ESPAsyncWebServer
 
-// Variable to store the HTTP request
-String header;
+// Set async web server port number to 80
+AsyncWebServer server(80);
+
+// Initialize vars
+const char *API_TOKEN = "api";
+
+// Initialize Webpage --> Website is saved in Flash not SRAM any more (PROGMEM)
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <title>Knusperpony Fan Counter</title>
+    <link rel=\"icon\" href=\"data:,\">
+    <style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
+    .button { background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none;
+    font-size: 30px; margin: 2px; cursor: pointer;}
+    </style>
+</head>
+
+<body>
+    <h1>Knusperpony Fan Counter</h1>
+    <label for=\"name\">Facebok API Token:</label>
+    <input type=\"text\" id=\"name\" name=\"name\">
+    <p><a href=\"/WifiReset\"><button class=\"button\">Delete Wifi Credentials</button></a></p>
+</body>
+
+</html>
+)rawliteral";
 
 // Function for Deleting WifiCredentials
 void EraseWifiCredentials()
@@ -23,6 +53,14 @@ void EraseWifiCredentials()
     ESP.restart();
     delay(300);
 }
+
+// If there is no Website give back 404
+void notFound(AsyncWebServerRequest *request)
+{
+    request->send(404, "text/plain", "Not found");
+}
+
+// Setup with Wifi Manager, open WifiManager when no AP is yet entered. Open own AP with "Knusperpony"
 void setup()
 {
     Serial.begin(115200);
@@ -36,84 +74,12 @@ void setup()
     wifiManager.autoConnect("KnusperPony");
     Serial.println("Connected.");
 
+    // Webpage
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send_P(200, "text/html", index_html); });
+
     server.begin();
 }
-
-// Initialize the ESP
 void loop()
 {
-    WiFiClient client = server.available(); // Listen for incoming clients
-
-    if (client)
-    {                                  // If a new client connects,
-        Serial.println("New Client."); // print a message out in the serial port
-        String currentLine = "";       // make a String to hold incoming data from the client
-        while (client.connected())
-        { // loop while the client's connected
-            if (client.available())
-            {                           // if there's bytes to read from the client,
-                char c = client.read(); // read a byte, then
-                Serial.write(c);        // print it out the serial monitor
-                header += c;
-                if (c == '\n')
-                { // if the byte is a newline character
-                    // if the current line is blank, you got two newline characters in a row.
-                    // that's the end of the client HTTP request, so send a response:
-                    if (currentLine.length() == 0)
-                    {
-                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                        // and a content-type so the client knows what's coming, then a blank line:
-                        client.println("HTTP/1.1 200 OK");
-                        client.println("Content-type:text/html");
-                        client.println("Connection: close");
-                        client.println();
-                        // if WiFiReset is called, call the EraseWifiCredentials
-                        if (header.indexOf("GET /WifiReset") >= 0)
-                        {
-                            Serial.println("Wifi Settings Deleted");
-                            EraseWifiCredentials();
-                        }
-
-                        // Display the HTML web page
-                        client.println("<!DOCTYPE html><html>");
-                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                        client.println("<link rel=\"icon\" href=\"data:,\">");
-                        // CSS to style the on/off buttons
-                        // Feel free to change the background-color and font-size attributes to fit your preferences
-                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-                        client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-                        client.println(".button2 {background-color: #77878A;}</style></head>");
-
-                        // Web Page Heading
-                        client.println("<body><h1>Knusperpony Fan Counter</h1>");
-                        client.println("<label for=\"name\">Facebok API Token:</label>");
-                        client.println("<input type=\"text\" id=\"name\" name=\"name\">");
-
-                        client.println("<p><a href=\"/WifiReset\"><button class=\"button\">Delete Wifi Credentials</button></a></p>");
-                        client.println("</body></html>");
-
-                        // The HTTP response ends with another blank line
-                        client.println();
-                        // Break out of the while loop
-                        break;
-                    }
-                    else
-                    { // if you got a newline, then clear currentLine
-                        currentLine = "";
-                    }
-                }
-                else if (c != '\r')
-                {                     // if you got anything else but a carriage return character,
-                    currentLine += c; // add it to the end of the currentLine
-                }
-            }
-        }
-        // Clear the header variable
-        header = "";
-        // Close the connection
-        client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");
-    }
 }
