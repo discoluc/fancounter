ae2169a5252adac0d4f0cf288f118d7f22b41263
snikcers <snikcers@gmail.com>
2022-09-18 18:13:35 +0200

    Sexond user + Matrix Display Introduced

--- fancounter.ino
+++ fancounter.ino
@@ -7,6 +7,7 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <Arduino_JSON.h>
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <ESPAsyncTCP.h> // https://github.com/me-no-dev/ESPAsyncTCP
 
@@ -14,26 +15,53 @@
 #define WEBSERVER_H
 #include <ESPAsyncWebServer.h> // https://github.com/me-no-dev/ESPAsyncWebServer
 
+// Use Little Fs instead SPIFFS
 #include <Hash.h>
-//#include <FS.h>
 #include <LittleFS.h>
 #define SPIFFS LittleFS
+
+// Fastled
+//#define FASTLED_ESP8266_D1_PIN_ORDER
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
+
+#include <Adafruit_GFX.h>
+#include <FastLED.h>
+#include <FastLED_NeoMatrix.h>
+
+const int MATRIX_PIN = D2;
+
 // Set async web server port number to 80
 AsyncWebServer server(80);
 
 // Initialize vars
 const char *API_KEY = "api_key";
 const char *USER_ID = "user_id";
+const char *ACCOUNT_NAME = "second_user";
 
 unsigned long lastTime = 0;
 // Set timer to 40 seconds (40000)
 unsigned long timerDelay = 40000;
 String graph_facebook = "https://graph.facebook.com/v14.0/";
 String api_follower = "?fields=followers_count&access_token=";
-String second_account = "ridersfuture";
-String api_business_discovery = "?fields=business_discovery.username(" + second_account + ")%7Bfollowers_count%7D&access_token=";
+String api_business_discovery = "?fields=business_discovery.username(";
+String api_business_discovery_follower = ")%7Bfollowers_count%7D&access_token=";
 String follower_JSON;
 String follower_request;
+String follower_request2;
+int follower_count_com;
+int follower_count;
+int follower_count2 = 0;
+String second_user = "";
+
+#define mw 8
+#define mh 32
+#define NUMMATRIX (mw * mh)
+
+CRGB matrixleds[NUMMATRIX];
+
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(matrixleds, mh, mw,
+                                                  NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG);
+
 // Initialize Webpage --> Website is saved in Flash not SRAM any more (PROGMEM)
 const char index_html[] PROGMEM =
     R"rawliteral(
@@ -68,7 +96,10 @@ const char index_html[] PROGMEM =
             alert("Saved User ID + API Key to file system.");
             setTimeout(function () { document.location.reload(false); }, 500);
         }
-
+        function submitMessage2() {
+            alert("Saved second account to file system.");
+            setTimeout(function () { document.location.reload(false); }, 500);
+        }
         function showPW() {
             var x = document.getElementById("api_key_field");
             if (x.type === "password") {
@@ -83,17 +114,21 @@ const char index_html[] PROGMEM =
 
 <body>
     <h1>Knusperpony Fan Counter</h1>
-    <label for="name">Facebok API Token:</label>
+    <label>Facebok API Token:</label>
     <form action="/set_api">
         <input type="text" id="user_id_field" name="user_id" value="%user_id%">
         <input type="password" id="api_key_field" name="api_key" value="%api_key%">
         <input type="submit" value="Submit" onclick="submitMessage()">
-    </form>
-    <br>
+    </form><br>
     <input type="checkbox" onclick="showPW()">Show API Key
+    <br><br><br>
     <br>
-    <br>
-    <br>
+    <label>Second User:</label>
+    <form action="/set_second_user">
+        <input type="text" id="second_user" name="second_user" value="%second_user%">
+        <input type="submit" value="Submit" onclick="submitMessage2()">
+    </form>
+
     <form action="/WifiReset">
         <button class="button">Delete Wifi Credentials</button>
     </form>
@@ -101,6 +136,10 @@ const char index_html[] PROGMEM =
 
 </html>
 )rawliteral";
+
+static const uint16_t bmpArray[64] PROGMEM =
+    {
+        0, 14783, 25023, 37375, 37375, 49598, 49598, 0, 35294, 35294, 65535, 65535, 65535, 65535, 63999, 63928, 53791, 65535, 63928, 63928, 63928, 65535, 65535, 63928, 61983, 65535, 63928, 65535, 65535, 63928, 65535, 63928, 59885, 65535, 59885, 65535, 65535, 63928, 65535, 63928, 64518, 65535, 64518, 64518, 64518, 59885, 65535, 63928, 64901, 65260, 65535, 65535, 65535, 65535, 59885, 63928, 0, 65461, 65461, 65166, 64967, 62502, 64166, 0};
 
 // Function for Deleting WifiCredentials
 void EraseWifiCredentials()
@@ -171,6 +210,10 @@ String processor(const String &var)
     {
         return readFile(SPIFFS, "/user_id.txt");
     }
+    else if (var == "second_user")
+    {
+        return readFile(SPIFFS, "/second_user.txt");
+    }
     return String();
 }
 
@@ -178,6 +221,7 @@ String httpGETRequest(const char *serverName)
 {
     WiFiClientSecure client;
     HTTPClient http;
+    // better solution, get root certificates from mozilla and use tsl handshake (set CPU to 160 MHZ)
     client.setInsecure();
     //  client.connect(host, httpsPort);
     //   Your IP address with path or Domain name with URL path
@@ -186,13 +230,13 @@ String httpGETRequest(const char *serverName)
     //  Send HTTP POST request
     int httpResponseCode = http.GET();
 
-    String payload = "{}";
+    String stringload = "{}";
 
     if (httpResponseCode > 0)
     {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
-        payload = http.getString();
+        stringload = http.getString();
     }
     else
     {
@@ -202,14 +246,14 @@ String httpGETRequest(const char *serverName)
     // Free resources
     http.end();
 
-    return payload;
+    return stringload;
 }
 
 // Setup with Wifi Manager, open WifiManager when no AP is yet entered. Open own AP with "Knusperpony"
 void setup()
 {
     Serial.begin(115200);
-    // Initialize SPIFFS/Filesystem
+    // Initialize SPIFFS/Filesystem (remember SPIFFS is now LittleFS)
 
     if (!SPIFFS.begin())
     {
@@ -228,8 +272,10 @@ void setup()
     Serial.println("Connected.");
     String api_key = readFile(SPIFFS, "/api_key.txt");
     String user_id = readFile(SPIFFS, "/user_id.txt");
+    second_user = readFile(SPIFFS, "/second_user.txt");
     follower_request = graph_facebook + user_id + api_follower + api_key;
-    Serial.println(follower_request);
+    follower_request2 = graph_facebook + user_id + api_business_discovery + second_user + api_business_discovery_follower + api_key;
+    Serial.println(follower_request2);
 
     // Start Landing Webpage, calls the processor function on the index_html
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
@@ -238,46 +284,89 @@ void setup()
     server.on("/WifiReset", HTTP_GET, [](AsyncWebServerRequest *request)
               { EraseWifiCredentials(); });
 
-    // Send a HTTP GET request to xxx.xxx.xxx/get?api_key=<inputMessage>
+    // Send a HTTP GET request to xxx.xxx.xxx/get?api_key=<inputMessage>&user_id=<input>
     server.on("/set_api", HTTP_GET, [](AsyncWebServerRequest *request)
               {
         String apimsg;
         String usermsg;
-        // GET api_key value on xxx.xxx.xxx/get?api_key=<inputMessage>
         if (request->hasParam(API_KEY)&& request->hasParam(USER_ID))
         {
             apimsg = request->getParam(API_KEY)->value();
             usermsg = request->getParam(USER_ID)->value();
             writeFile(SPIFFS, "/api_key.txt", apimsg.c_str());
-            writeFile(SPIFFS, "/user_id.txt", usermsg.c_str()); 
+            writeFile(SPIFFS, "/user_id.txt", usermsg.c_str());
+            String second_user = readFile(SPIFFS, "/second_user.txt");  
             follower_request = graph_facebook + usermsg + api_follower + apimsg;
-            
+            follower_request2 = graph_facebook + usermsg + api_business_discovery + second_user + api_business_discovery_follower + apimsg;
+ 
 
         }
         else
         {
             apimsg = "No message sent";
         } });
+    // Send a HTTP GET request to xxx.xxx.xxx/get?api_key=<inputMessage>&user_id=<input>
+    server.on("/set_second_user", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+        String secondusermsg;
+        if (request->hasParam(ACCOUNT_NAME))
+        {
+            secondusermsg = request->getParam(ACCOUNT_NAME)->value();
+            if (secondusermsg.length()==0){
+                secondusermsg = " ";
+            }
+            writeFile(SPIFFS, "/second_user.txt", secondusermsg.c_str());
+            String api_key = readFile(SPIFFS, "/api_key.txt");
+            String user_id = readFile(SPIFFS, "/user_id.txt");
+            second_user = secondusermsg;
+            follower_request2 = graph_facebook + user_id + api_business_discovery + secondusermsg + api_business_discovery_follower + api_key;
+            Serial.println(follower_request2);
+            
+
+        }
+        else
+        {
+            secondusermsg = "No message sent";
+        } });
+
     server.onNotFound(notFound);
     server.begin();
+    FastLED.addLeds<NEOPIXEL, MATRIX_PIN>(matrixleds, NUMMATRIX);
+    matrix->begin();
+    matrix->setTextWrap(false);
+    matrix->setBrightness(50);
+    matrix->setTextColor(matrix->Color(255, 255, 255));
+    matrix->clear();
 }
 
 void loop()
 {
-
     // Send an HTTP POST request depending on timerDelay
-    if ((millis() - lastTime) > timerDelay)
+    if ((millis() + 40001 - lastTime) > timerDelay)
     {
         // Check WiFi connection status
         if (WiFi.status() == WL_CONNECTED)
         {
             follower_JSON = httpGETRequest(follower_request.c_str());
-            Serial.println(follower_JSON);
+            follower_count = JSON.parse(follower_JSON)["followers_count"];
+            Serial.println(second_user.length());
+            if (second_user.length() >= 3)
+            {
+                follower_count2 = JSON.parse(httpGETRequest(follower_request2.c_str()))["followers_count"];
+            }
+            follower_count_com = follower_count + follower_count2;
+            Serial.println(follower_count_com);
         }
         else
         {
             Serial.println("WiFi Disconnected");
         }
-        lastTime = millis();
+        lastTime = millis() + 40001;
+        matrix->clear();
     }
+    matrix->drawRGBBitmap(0, 0, bmpArray, 8, 8);
+    matrix->setCursor(9, 1);
+    matrix->print(follower_count_com);
+    matrix->show();
+    delay(400);
 }
